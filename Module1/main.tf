85fed4a8421619fa2ad9e99fc1f3b1788585413a
divyanshursahu <96013623+divyanshursahu@users.noreply.github.com>
2023-05-26 17:45:33 +0530

    New files

--- Module1/main.tf
+++ Module1/main.tf
@@ -1,1 +1,31 @@
+terraform {
+  required_providers {
+    azurerm{
+        source = "hashicorp/azurerm"
+        version = "3.57.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  
+}
+
+resource "azurerm_resource_group" "rg-name" {
+  name = "ContosoResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name = "CoreServicesVnet"
+  location = "East US"
+  resource_group_name = azurerm_resource_group.rg-name.name
+  address_space = ["10.20.0.0/16"]
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name = "ManufacturingVnet"
+  location = "West Europe"
+  resource_group_name = azurerm_resource_group.rg-name.name
+  address_space = ["10.30.0.0/16"]
+}
